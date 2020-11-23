7aebded661a7d02415a1afa66defbc7d3939deff
Steve Mudge <mudge.steve@gmail.com>
2020-11-22 18:13:57 -0800

    My 2nd commit changes for ampifyapp v2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,68 @@ import navBar from '../src/myNavBar';
 import './App.css';
 
 function App() {
+  const myCss = `.App {
+    text-align: center;
+  }
+  
+  .container-fluid {
+    margin: 0;
+  
+  }
+  #header {
+    background-color: #498cac;
+    min-height: 50px;
+  }
+  
+  #main-wrapper {
+    margin: 10px auto;
+    min-height: 500px;
+    width: 90%;
+    border: 1px solid black;
+    background-color: lightgray;
+  }
+  
+  #main {
+    margin: 10px auto 10px auto;
+    padding: 0px;
+    background-color: lightslategray;
+  }
+  
+  #leftSideBar {
+    width: 50px;
+    border: 1px solid red;
+    background-color: lightgreen;
+  }
+  
+  #content {
+    margin: 0px auto 0px auto;
+    background-color: rgb(55, 95, 146);
+  }
+  
+  #rightSideBar {
+    width: 10%;
+    background-color: lightgreen;
+  }
+  
+  #footer {
+    min-height: 50px;
+    background-color: darkgray;
+  }
+  
+  .section {
+    margin: 5px 5px;
+    border: 2px dashed darkgoldenrod;
+    background-color: darkgray;
+  }
+  
+  .myRow {
+    background-color: lightblue;
+  }`
+
   return (
-    <div className="App">
+    <div className="Steve using Amplify">
       <nav>
-        <navBar class="row justify-content-between align-items-center myRow">
+        <navBar id="navBar" class="row justify-content-between align-items-center myRow">
           <div className="col-sm-2 btn btn-primary">1</div>
           <div className="col-sm-2 btn btn-info">2</div>
           <div className="col-sm-2 btn btn-warning">3</div>
@@ -14,7 +72,7 @@ function App() {
         </navBar>
       </nav>
 
-      <header className="row App-header justify-content-center align-items-center">
+      <header id="header" className="row App-header justify-content-center align-items-center">
         <div className="row justify-content-between align-items-center">
           <div className="col-xl-12">
             Header  
@@ -24,10 +82,23 @@ function App() {
 
       <main id="main-wrapper" className="container-fluid">
         container with three columns
-        <div id="main" className="row">
-          <div id="leftSideBar" className="col">leftSideBar</div>
-          <div id="content" className="col">Main Content</div>
-          <div id="rightSideBar" className="col">rightSideBar</div>
+        <div id="main" className="row justify-content-between align-items-center">
+          <div id="leftSideBar" className="col-sm-2">leftSideBar</div>
+          <div id="content" className="row col-sm-8 container justify-content-center align-items-center">
+              <div id="section-head" className="col-sm-12">
+                Main
+              </div>
+              <div id="section1" className="col-sm-12 section">
+                ${myCss}
+              </div>
+              <div id="section2" className="col-sm-12 section">
+                Section 2
+              </div>
+              <div id="section3" className="col-sm-12 section">
+                Section 3
+              </div>
+          </div>
+          <div id="rightSideBar" className="col-sm-2">rightSideBar</div>
         </div>
       </main>
 
