b0363b9e517f137997c5f3195feb0169a2d08b30
Steve Mudge <mudge.steve@gmail.com>
2020-11-22 17:12:11 -0800

    My 1st commit for ampifyapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import navBar from '../src/myNavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <navBar class="row justify-content-between align-items-center myRow">
+          <div className="col-sm-2 btn btn-primary">1</div>
+          <div className="col-sm-2 btn btn-info">2</div>
+          <div className="col-sm-2 btn btn-warning">3</div>
+          <div className="col-sm-2 btn btn-info">4</div>
+          <div className="col-sm-2 btn btn-primary">5</div>
+        </navBar>
+      </nav>
+
+      <header className="row App-header justify-content-center align-items-center">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-xl-12">
+            Header  
+          </div>
+        </div>
+      </header>   
+
+      <main id="main-wrapper" className="container-fluid">
+        container with three columns
+        <div id="main" className="row">
+          <div id="leftSideBar" className="col">leftSideBar</div>
+          <div id="content" className="col">Main Content</div>
+          <div id="rightSideBar" className="col">rightSideBar</div>
+        </div>
+      </main>
+
+      <footer>
+        <div id="footer" className="row justify-content-center align-items-center">footer</div>
+      </footer>
     </div>
   );
 }
